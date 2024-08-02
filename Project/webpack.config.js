612afddbf50bf14d177b29004b5b875ec00e0b08
Sumanbassetti <128141423+Sumanbassetti@users.noreply.github.com>
2024-08-02 13:11:13 +0530

    [UPD] Webpack Build

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const envMode = process.env.NODE_ENV || "development" //"development" | "production" 
 
 module.exports = {
@@ -14,15 +13,5 @@ module.exports = {
     node: {
         __dirname: false
     },
-    plugins: [
-        // new CopyWebpackPlugin({
-        //     patterns: [
-        //         './node_modules/swagger-ui-dist/swagger-ui.css',
-        //         './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
-        //         './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
-        //         './node_modules/swagger-ui-dist/favicon-16x16.png',
-        //         './node_modules/swagger-ui-dist/favicon-32x32.png'
-        //     ]
-        // })
-    ]
+    plugins: [ ]
 };
